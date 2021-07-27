7198e3791709b97cbe58af13933f982aef886442
echoaman <amanpahurkar13@gmail.com>
2021-07-28 02:22:14 +0530

    res 28/7/21

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="author" content="Aman Pahurkar" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          </Head>
+          <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import pic from '../public/me.jpg';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
     function approxExp() {
@@ -15,24 +16,26 @@ export default function Home() {
     }
 
     return (
-        <div className="container">
+        <div className={styles.container}>
             <Head>
-                <title>Aman Pahurkar</title>
-                <meta name="description" content="Personal website" />
+                <title>Aman Pahurkar | Software Engineer</title>
+                <link rel="canonical" href="https://amanpahurkar.vercel.app" />
+                <meta name="description" content="I am Aman Pahurkar and welcome to my personal website! I am currently working at CarWale as an Associate Software Development Engineer. Feel free to reach out to me." />
+                <meta name="keywords" content="personal website, software engineer, computer engineering, B.Tech"/>
             </Head>
-            <div id="me">
+            <div id={styles.me}>
                 <Image 
                     src={pic}
-                    width={200}
-                    height={200}
+                    width={1000}
+                    height={1000}
                     layout="intrinsic"
-                    alt="Aman Pahurkar"
+                    alt="Image of Aman Pahurkar"
                 />
             </div>
 
-            <div className="name-container">
+            <header className={styles.nameContainer}>
                 <svg
-                    id="name"
+                    id={styles.name}
                     viewBox="0 0 439 51"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -110,11 +113,11 @@ export default function Home() {
                     mask="url(#path-1-outside-1)"
                 />
                 </svg>
-            </div>
+            </header>
 
-            <p id="about">Hello! I am a Software Engineer with about {approxExp()} months of experience. Currently I am working at <a href={"https://carwale.com"}>CarWale</a> as an Associate Software Development Engineer. Prior to this, I was an intern at CarWale.</p>
+            <p id={styles.about}>Hello! I am a Software Engineer with about {approxExp()} months of experience. Currently I am working at <a href="https://carwale.com">CarWale</a> as an Associate Software Development Engineer. Prior to this, I was an intern at CarWale.</p>
             
-            <div id="links">
+            <div id={styles.links}>
                 <a href="https://github.com/echoaman" target="_blank" rel="noopener noreferrer">
                     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path fill="#FFF" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
                 </a>
@@ -124,7 +127,7 @@ export default function Home() {
                 <a href="mailto:amanpahurkar13@gmail.com">
                     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Mail</title><path fill="#EA4335" d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>
                 </a>
-                <a href="https://drive.google.com/file/d/1g_kJTqqfjysOrFarRHfRlL8h13mn9lr3/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1sUltNHSunJr-g2jgxFM6c8RvC6Ec9yR5/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4 -2 24 24" preserveAspectRatio="xMinYMin"><title>Resume</title><path fill="#FFF" d="M3 0h10a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3a3 3 0 0 1 3-3zm8 2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2h-2zM3 9a1 1 0 1 0 0 2h10a1 1 0 0 0 0-2H3zm0 3a1 1 0 0 0 0 2h10a1 1 0 0 0 0-2H3zm0 3a1 1 0 0 0 0 2h5a1 1 0 0 0 0-2H3zm8-10a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2h-2zM3 2a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3z"></path></svg>
                 </a>
             </div>

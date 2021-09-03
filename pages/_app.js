d12124c5b8263c2caa168691daf89dca7cb645b2
echoaman <amanpahurkar13@gmail.com>
2021-09-04 02:42:56 +0530

    sitemap and google verification

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
             <link rel="icon" href="/favicon.ico" />
             <meta name="author" content="Aman Pahurkar" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="google-site-verification" content="Z5ESajq8T3JxcQ45GBL8XR9FTDVRS-DTRYygaZbxBhI" />
           </Head>
           <Component {...pageProps} />
         </>

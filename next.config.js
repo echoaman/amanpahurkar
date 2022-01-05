30d41413635a945f5c8f22666963bb4b650772bc
echoaman <amanpahurkar13@gmail.com>
2022-01-06 02:15:10 +0530

    res 22/1/6

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/resume",
+				destination: "/AmanPahurkar.pdf",
+				permanent: true
+			}
+		]
+	}
 }

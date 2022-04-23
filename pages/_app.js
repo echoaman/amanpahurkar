80d1ca8215fd2daa26903074ec0659a7ed5a1248
echoaman <amanpahurkar13@gmail.com>
2022-04-23 16:04:29 +0530

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="author" content="Aman Pahurkar" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                {/* <meta name="google-site-verification" content="Z5ESajq8T3JxcQ45GBL8XR9FTDVRS-DTRYygaZbxBhI" /> */}
+                <meta name="google-site-verification" content="Z5ESajq8T3JxcQ45GBL8XR9FTDVRS-DTRYygaZbxBhI" />
             </Head>
             <Component {...pageProps} />
         </Layout>

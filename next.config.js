46981b79b21d2103464b9656c8c740658bc60724
echoaman <amanpahurkar13@gmail.com>
2022-08-21 02:27:37 +0530

    csp srcipt-src

--- next.config.js
+++ next.config.js
@@ -44,7 +44,7 @@ module.exports = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self' ; script-src 'self' 'unsafe-eval' 'unsafe-inline' ; style-src * 'unsafe-inline' ; font-src * ; img-src * data:"
+                        value: "default-src 'self' ; script-src * 'unsafe-eval' 'unsafe-inline' ; style-src * 'unsafe-inline' ; font-src * ; img-src * data:"
                     }
                 ]
             }
